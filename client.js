43f4fda20db19ec715b0a4c41cd298cb9480214c
Paul Goins <general@vultaire.net>
2015-02-12 06:56:14 -0800

    Reworked code a bit.  Handling line buffering a little better...

--- client.js
+++ client.js
@@ -1,9 +1,6 @@
 var Client = function () {
     'use strict';
 
-    var currentCommand = [];
-    var charBuffer = [];
-
     var commandMap = {
         240: 'SE',
         241: 'NOP',
@@ -41,7 +38,11 @@ var Client = function () {
         continueLine: false,  // Probably not used so often with MUDs...
         passwordPrompt: false,
         shouldAutoScroll: true,
+        currentCommand: null,
+        outputBuffer: null,
         initialize: function (outputElem) {
+            this.currentCommand = [];
+            this.outputBuffer = "";
             this.outputElem = outputElem;
             var client = this;
             this.outputElem.addEventListener('scroll', function () {
@@ -76,49 +77,51 @@ var Client = function () {
             fr.readAsArrayBuffer(event.data);
         },
         handleMessage: function (buffer) {
+            var rawOutputBuffer = []
             var uint8 = new Uint8Array(buffer, 0, buffer.length);
             for (var i=0; i<uint8.length; i++) {
                 var byte = uint8[i];
-                if (currentCommand.length === 0) {
+                if (this.currentCommand.length === 0) {
                     if (byte === 255) {  // Interpret as Command (IAC)
-                        currentCommand.push(byte);
+                        this.currentCommand.push(byte);
                     } else {
-                        charBuffer.push(byte);
+                        rawOutputBuffer.push(byte);
                     }
                 } else {
-                    currentCommand.push(byte);
-                    if (currentCommand.length === 2) {
+                    this.currentCommand.push(byte);
+                    if (this.currentCommand.length === 2) {
                         if (byte < 240) {
                             // Invalid telnet command
-                            console.log(currentCommand.map(commandToString));
-                            currentCommand = [];
+                            console.log(this.currentCommand.map(commandToString));
+                            this.currentCommand = [];
                         } else if (240 <= byte && byte <= 250) {
                             // Valid 2-byte telnet command
-                            console.log(currentCommand.map(commandToString));
+                            console.log(this.currentCommand.map(commandToString));
                             // TO DO: do something with this ;)
-                            currentCommand = [];
+                            this.currentCommand = [];
                         } else if (251 <= byte && byte <= 254) {
                             // Second byte of a 3-byte command; do nothing yet
                         } else if (byte === 255) {
                             // Literal IAC
                             // TO DO: do something with this ;)
                             // ... likely unneeded for Aard...?
-                            currentCommand = [];
+                            this.currentCommand = [];
                         }
-                    } else if (currentCommand.length === 3) {
-                        console.log(currentCommand.map(commandToString));
+                    } else if (this.currentCommand.length === 3) {
+                        console.log(this.currentCommand.map(commandToString));
                         // TO DO: do something with this ;)
-                        currentCommand = [];
+                        this.currentCommand = [];
                     }
                     // Try to interpret command
                 }
             }
-            var output = String.fromCharCode.apply(null, charBuffer);
-            charBuffer = [];
+            var output = String.fromCharCode.apply(null, rawOutputBuffer);
             this.pushOutput(output);
         },
         pushOutput: function (output) {
             var client = this;
+            // Precede output with any leftover bits from previous packets
+            output = client.outputBuffer + output;
             var lines = output.split('\n\r');
             lines.forEach(function (line) {
                 var lineElem = client.currentLine;
@@ -129,6 +132,10 @@ var Client = function () {
                         lineElem.innerHTML = '&nbsp;';
                         return;
                     }
+                }
+                if (line.charAt(line.length-1) === '\n') {
+                    client.outputBuffer = '\n';
+                    line = line.slice(0, line.length-2);
                 }
                 client.appendLine(line);
 
