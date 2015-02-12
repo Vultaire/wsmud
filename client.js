06f5aa09f8e34d0c7045fc67518cd4e6097adf86
Paul Goins <general@vultaire.net>
2015-02-12 05:45:19 -0800

    MUD output now goes to the main window.

--- client.js
+++ client.js
@@ -32,72 +32,95 @@ var Client = function () {
         return commandMap[command];
     };
 
-    var handleMessage = function (buffer) {
-        var uint8 = new Uint8Array(buffer, 0, buffer.length);
-        for (var i=0; i<uint8.length; i++) {
-            var byte = uint8[i];
-            if (currentCommand.length === 0) {
-                if (byte === 255) {  // Interpret as Command (IAC)
-                    currentCommand.push(byte);
-                } else {
-                    charBuffer.push(byte);
-                }
-            } else {
-                currentCommand.push(byte);
-                if (currentCommand.length === 2) {
-                    if (byte < 240) {
-                        // Invalid telnet command
-                        console.log(currentCommand.map(commandToString));
-                        currentCommand = [];
-                    } else if (240 <= byte && byte <= 250) {
-                        // Valid 2-byte telnet command
-                        console.log(currentCommand.map(commandToString));
-                        // TO DO: do something with this ;)
-                        currentCommand = [];
-                    } else if (251 <= byte && byte <= 254) {
-                        // Second byte of a 3-byte command; do nothing yet
-                    } else if (byte === 255) {
-                        // Literal IAC
-                        // TO DO: do something with this ;)
-                        // ... likely unneeded for Aard...?
-                        currentCommand = [];
-                    }
-                } else if (currentCommand.length === 3) {
-                    console.log(currentCommand.map(commandToString));
-                    // TO DO: do something with this ;)
-                    currentCommand = [];
-                }
-                // Try to interpret command
-            }
-        }
-        var output = String.fromCharCode.apply(null, charBuffer);
-        console.log(output);
-        charBuffer = [];
-    };
-
-    var onMessage = function (event) {
-        fr = new FileReader();
-        fr.addEventListener('loadend', function () {
-            return handleMessage(fr.result);
-        });
-        fr.readAsArrayBuffer(event.data);
-    };
-
     return {
         socket: null,
         outputElem: null,
+        currentLine: null,
+        continueLine: false,
         initialize: function (outputElem) {
             this.outputElem = outputElem;
         },
         connect: function (addr) {
             this.socket = new WebSocket(addr, ["telnet"]);
-            this.socket.addEventListener('message', onMessage);
+            this.socket.addEventListener('message', this.onMessage.bind(this));
             return this.socket;
         },
         onUserInput: function (userInput) {
             // TO DO: Echo output
             // (Except for password...)
             this.socket.send(userInput + '\n');
+        },
+        onMessage: function (event) {
+            fr = new FileReader();
+            var client = this;
+            fr.addEventListener('loadend', function () {
+                return client.handleMessage(fr.result);
+            });
+            fr.readAsArrayBuffer(event.data);
+        },
+        handleMessage: function (buffer) {
+            var uint8 = new Uint8Array(buffer, 0, buffer.length);
+            for (var i=0; i<uint8.length; i++) {
+                var byte = uint8[i];
+                if (currentCommand.length === 0) {
+                    if (byte === 255) {  // Interpret as Command (IAC)
+                        currentCommand.push(byte);
+                    } else {
+                        charBuffer.push(byte);
+                    }
+                } else {
+                    currentCommand.push(byte);
+                    if (currentCommand.length === 2) {
+                        if (byte < 240) {
+                            // Invalid telnet command
+                            console.log(currentCommand.map(commandToString));
+                            currentCommand = [];
+                        } else if (240 <= byte && byte <= 250) {
+                            // Valid 2-byte telnet command
+                            console.log(currentCommand.map(commandToString));
+                            // TO DO: do something with this ;)
+                            currentCommand = [];
+                        } else if (251 <= byte && byte <= 254) {
+                            // Second byte of a 3-byte command; do nothing yet
+                        } else if (byte === 255) {
+                            // Literal IAC
+                            // TO DO: do something with this ;)
+                            // ... likely unneeded for Aard...?
+                            currentCommand = [];
+                        }
+                    } else if (currentCommand.length === 3) {
+                        console.log(currentCommand.map(commandToString));
+                        // TO DO: do something with this ;)
+                        currentCommand = [];
+                    }
+                    // Try to interpret command
+                }
+            }
+            var output = String.fromCharCode.apply(null, charBuffer);
+            charBuffer = [];
+            this.pushOutput(output);
+        },
+        pushOutput: function (output) {
+            console.log(output.split('\n\r'));  // For debug and comparison
+            var client = this;
+            var lines = output.split('\n\r');
+            lines.forEach(function (line) {
+                console.log([line, line.charCodeAt(0), line.charCodeAt(1), line.charCodeAt(2)]);
+                if (!client.continueLine) {
+                    lineElem = document.createElement('div');
+                    lineElem.classList.add('output-line');
+                    client.outputElem.appendChild(lineElem);
+                    client.currentLine = lineElem;
+
+                    if (line.trim().length === 0) {
+                        // Workaround for empty lines
+                        lineElem.innerHTML = '&nbsp;';
+                        return;
+                    }
+                }
+                lineElem.innerHTML += _.template('<%- line %>')({line: line});
+                // TO DO: determine when to continue a previous line...
+            });
         },
     };
 }();
--- index.js
+++ index.js
@@ -1,4 +1,24 @@
+var debug;
+
 (function () {
+    "use strict";
+
+    var onResize = function () {
+        var inputDiv = document.querySelector('div.input');
+        var outputDiv = document.querySelector('div.output');
+        // I don't like the 5 pixel offsets...
+        outputDiv.style.height = sprintf(
+            "%dpx", window.innerHeight - inputDiv.offsetHeight);
+        outputDiv.style.width = sprintf("%dpx", window.innerWidth);
+        // Input width: a little trickier due to input box's overflow.
+        var inputElem = inputDiv.querySelector('input');
+        var widthOffset = inputElem.offsetWidth - inputElem.clientWidth;
+        inputDiv.style.width = sprintf("%dpx", window.innerWidth - widthOffset);
+    };
+
+    window.addEventListener('resize', function () {
+        onResize();
+    });
     document.addEventListener('DOMContentLoaded', function () {
         var client = Object.create(Client);
         client.initialize(document.querySelector('div.output'));
@@ -15,7 +35,10 @@
                 client.onUserInput(inputElem.value);
             }
         });
+        onResize();
+        inputElem.focus();
 
+        if (true) {
         var socket = client.connect("ws://localhost:50008/");
         socket.addEventListener('open', function (e) {
             console.log('Web socket opened successfully.');
@@ -23,5 +46,6 @@
         socket.addEventListener('error', function (e) {
             alert('Could not open web socket!');
         });
+        }
     });
 })();
